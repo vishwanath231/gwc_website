1c0c2f23ed9cc51588647ac7e5e90205bdc450c2
vishwanath231 <vishwanatharuchunan@gmail.com>
2023-09-04 13:11:15 +0530

    career page

---
+++ src/screens/career/_Career.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './components/career.css';
+import Navbar from '../../components/navbar/Navbar';
+// import Footer from '../../components/footer/Footer';
+
+
+const _Career = () => {
+    return (
+        <>
+            <Navbar />
+            {/* <Footer /> */}
+        </>
+    )
+}
+
+export default _Career;

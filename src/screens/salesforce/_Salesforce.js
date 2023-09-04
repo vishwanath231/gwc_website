09da29ce3098bec66d68a03bf5f9fa01a7b66c16
vishwanath231 <vishwanatharuchunan@gmail.com>
2023-09-04 12:01:45 +0530

    saleforce page

--- src/screens/salesforce/_Salesforce.js
+++ src/screens/salesforce/_Salesforce.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Navbar from '../../components/navbar/Navbar';
 import './components/salesforce.css';
 import SalesforceBanner from './components/SalesforceBanner';
+import SaleforceMain from './components/SaleforceMain';
+import Footer from '../../components/footer/Footer';
+
 
 const _Salesforce = () => {
     return (
         <>
             <Navbar />
             <SalesforceBanner />
+            <SaleforceMain />
+            <Footer />
         </>
     )
 }
---
+++ src/screens/salesforce/components/SaleforceMain.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import SalesMen from '../../../assets/salesforce/4-machine-learning-header.png';
+import SaleforceInfo from '../../../assets/salesforce/crm-main.gif';
+import SaleforceDesc from '../../../assets/salesforce/DK-blog-Einstein.png';
+
+
+const SaleforceMain = () => {
+    return (
+        <div>
+            <div className="max-w-screen-xl mx-auto my-5 py-20 px-5">
+                <div className="grid grid-cols-3 gap-5 items-center">
+                    <div className="col-span-2">
+                        <div className="text-2xl font-bold text-[#0A9DDA]">Unlocking The Power Of Salesforce With GWC Data Analytics</div>
+                        <div className="mt-3 text-[15px] leading-7">Are you looking to maximize the potential of Salesforce for your business? Look no further! GWC, a leading Data Analytics Company, is here to provide you with cutting-edge data solutions that will transform the way you leverage Salesforce. Our dedicated Salesforce COE is equipped with top-notch experts who specialize in harnessing the full capabilities of this robust CRM platform.</div>
+                    </div>
+                    <div className="">
+                        <img src={SalesMen} alt="" className='w-80' />
+                    </div>
+                </div>
+
+            </div>
+            <div className="bg-[#0A9DDA]">
+                <div className="max-w-screen-xl mx-auto  py-20 px-5">
+                    <div className="text-white font-medium text-2xl">Why Choose GWC for Salesforce?</div>
+                    <div className="mt-5 text-gray-100 leading-7">At GWC, we understand the importance of harnessing the power of data in today’s digital landscape. Our team of skilled professionals is committed to helping your business gain a competitive edge by utilizing Salesforce’s extensive suite of capabilities. With our proven expertise in data analytics and Salesforce, we can deliver tailored solutions that meet your unique business requirements.</div>
+                </div>
+            </div>
+            <div className="">
+                <div className="max-w-screen-xl mx-auto  py-20 px-5">
+                    <div className="text-black font-bold text-2xl mb-10">Salesforce Capabilities at GWC</div>
+                    <div className="grid grid-cols-3 gap-4">
+                        <div className="shadow p-4 mt-20 h-fit bg-[#FDFEFE] rounded-lg">
+                            <div className="text-lg text-[#042D60] font-bold mt-2 mb-4">Customization and Configuration</div>
+                            <div className="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span className='text-lg'>O</span>ur Salesforce COE team has deep expertise in customizing and configuring Salesforce to align with your specific business processes. From customizing fields and layouts to developing automation workflows, we ensure that Salesforce is tailored to fit your unique needs.</div>
+                        </div>
+                        <div className="shadow p-4 h-fit bg-[#FDFEFE] rounded-lg">
+                            <div className="text-lg text-[#042D60] font-bold mt-2 mb-4">Integration</div>
+                            <div className="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span className='text-lg'>W</span>e specialize in integrating Salesforce with various third-party systems, such as ERP, marketing automation, and customer support platforms. This enables seamless data flow and enhances your ability to make data-driven decisions across departments.</div>
+                        </div>
+                        <div className="shadow p-4 h-fit mt-20 bg-[#FDFEFE] rounded-lg">
+                            <div className="text-lg text-[#042D60] font-bold mt-2 mb-4">Analytics and Reporting</div>
+                            <div className="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span className='text-lg'>U</span>nlock the full potential of your data with our advanced analytics and reporting solutions. We help you build insightful dashboards, generate comprehensive reports, and leverage artificial intelligence to uncover actionable insights that drive growth.</div>
+                        </div>
+                    </div>
+                    <div className="flex justify-center items-center -mt-20">
+                        <img src={SaleforceInfo} alt="" className='w-96' />
+                    </div>
+                    <div className="grid grid-cols-3 gap-4">
+                        <div className="shadow p-4 -mt-40 h-fit bg-[#FDFEFE] rounded-lg">
+                            <div className="text-lg text-[#042D60] font-bold mt-2 mb-4">Sales and Marketing Automation</div>
+                            <div className="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span className='text-lg'>S</span>treamline your sales and marketing efforts with Salesforce’s powerful automation capabilities. Our experts can assist you in optimizing lead generation, managing customer journeys, and automating repetitive tasks, empowering your teams to focus on high-value activities.</div>
+                        </div>
+                        <div className="shadow p-4 h-fit bg-[#FDFEFE] rounded-lg">
+                            <div className="text-lg text-[#042D60] font-bold mt-2 mb-4">Mobile and App Development</div>
+                            <div className="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span className='text-lg'>T</span>ake your Salesforce experience on the go with our mobile and app development expertise. We can create custom mobile apps that enable your workforce to access critical data, collaborate effectively, and stay productive while on the move.</div>
+                        </div>
+                        <div className="shadow p-4 -mt-40 h-fit bg-[#FDFEFE] rounded-lg">
+                            <div className="text-lg text-[#042D60] font-bold mt-2 mb-4">Training and Support</div>
+                            <div className="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span className='text-lg'>W</span>e believe in empowering our clients to make the most of Salesforce. Our COE offers comprehensive training programs and ongoing support to ensure your teams are equipped with the knowledge and skills to maximize the benefits of Salesforce.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="saleforce_gwc">
+                <div className="max-w-screen-xl mx-auto  py-20 px-5">
+                    <div className="text-2xl text-[#042D60] font-bold">Why Optimize Your Salesforce Experience with GWC?</div>
+                    <div className="grid grid-cols-2 gap-5 mt-10">
+                        <div className="shadow saleforce_gwc_info p-4">
+                            <div className="text-lg font-bold mt-2 mb-4">Expertise</div>
+                            <div className="">Our Salesforce COE team comprises highly skilled professionals with extensive experience in data analytics and Salesforce implementation. We stay updated with the latest industry trends and best practices to deliver innovative solutions that drive business growth.</div>
+                        </div>
+                        <div className="shadow saleforce_gwc_info p-4">
+                            <div className="text-lg font-bold mt-2 mb-4">Tailored Solutions</div>
+                            <div className="">We understand that each business has unique requirements. Our team takes a consultative approach, working closely with you to understand your goals and deliver customized Salesforce solutions that align with your specific needs.</div>
+                        </div>
+                        <div className="shadow saleforce_gwc_info p-4">
+                            <div className="text-lg font-bold mt-2 mb-4">Data-Driven Insights</div>
+                            <div className="">With our expertise in data analytics, we help you unlock the hidden potential of your data within Salesforce. By leveraging advanced analytics and AI, we provide actionable insights that fuel strategic decision-making.</div>
+                        </div>
+                        <div className="shadow saleforce_gwc_info p-4">
+                            <div className="text-lg font-bold mt-2 mb-4">Proven Track Record</div>
+                            <div className="">GWC has a successful track record of delivering high-quality Salesforce solutions to a diverse range of clients. Our customer testimonials speak for themselves, showcasing our commitment to excellence and customer satisfaction.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-[#FDFEFE]">
+                <div className="max-w-screen-xl mx-auto mt-20 px-5">
+                    <div className="grid grid-cols-3 gap-4 items-center">
+                        <div className="col-span-2">
+                            <div className="">Ready to transform the way you do business? Partner with GWC’s Salesforce COE and harness the full power of Salesforce’s capabilities. Our data-driven solutions and expertise will empower you to make informed decisions, drive growth, and stay ahead in today’s competitive landscape.</div>
+                            <div className="mt-3"><a href="#" target='_blank' className='font-bold underline'>Contact us</a> today to learn more about how GWC can optimize your Salesforce experience and take your business to new heights.</div>
+                        </div>
+                        <div className="mx-auto my-0">
+                            <img src={SaleforceDesc} alt="" className='w-80' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SaleforceMain;
